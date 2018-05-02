389b42a69b10beb1044e2ce9c41d30545cefea34
Jim Barlow <jim@jimbarlow.com>
2018-05-02 08:57:42 -0400

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /home/jim/ansible-db-demo
+ansible-playbook -i cinventory registration.yml -u devops -b --ask-vault-pass
+ansible-playbook -i cinventory yumupdate.yml -u devops -b --ask-vault-pass
 cd ../mariadb-ansible-galera-cluster
 ansible-playbook -i galera.chosts galera.yml --tags setup -u devops -b
 ansible-playbook -i galera.chosts galera.yml --skip-tags setup -u devops -b
