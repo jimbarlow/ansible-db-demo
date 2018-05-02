b8fc8e9bf437e75a07b3537d2831263ea23047f4
Jim Barlow <jim@jimbarlow.com>
2018-05-02 11:47:05 -0400

    dbinserts.sh expanded

--- dbinserts.sh
+++ dbinserts.sh
@@ -1,6 +1,60 @@
+#!/bin/bash
+#-x
+echo""
+read -p "Press any key to perform datbase inserts" -n 1 -s
 mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (1, "Kraft", "Christopher", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
 mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (2, "Cernan", "Eugene", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
 mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (3, "Krantz", "Eugene", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
 mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (4, "McDivitt", "James", "Houston", "TX");'
-mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (5, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (5, "Collins", "Mike", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (6, "Charlesworth", "Cliff", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (7, "Aaron", "John", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (8, "Griffin", "Jerry", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (9, "Hodge", "John", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (10, "Puddy", "Don", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (11, "Lewis", "Charles", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (12, "Frank", "Pete", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (13, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (14, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (15, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (16, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Press any key to continue inserts" -n 1 -s
+mysql -u haproxy --host=192.168.144.20 -e 'insert into NASA.persons ( id, LastName, FirstName, City, State) values (17, "Lunney", "Glenn", "Houston", "TX");'
+echo""
+read -p "Done" -n 1 -s
+
+
+
+
 
